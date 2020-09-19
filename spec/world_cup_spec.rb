e9252519bbdae995674730817ce81181574a9713
Ryan Camp <campryan@comcast.net>
2020-09-19 13:39:16 -0600

    Write and pass existence test for WorldCup class

---
+++ spec/world_cup_spec.rb
@@ -0,0 +1,21 @@
+RSpec.describe WorldCup do
+  before do
+    @france = Team.new("France")
+    @mbappe = Player.new({name: "Kylian Mbappe", position: "forward"})
+    @pogba = Player.new({name: "Paul Pogba", position: "midfielder"})
+    @france.add_player(@mbappe)
+    @france.add_player(@pogba)
+
+    @croatia = Team.new("Croatia")
+    @modric = Player.new({name: "Luka Modric", position: "midfielder"})
+    @vida = Player.new({name: "Domagoj Vida", position: "defender"})
+    @croatia.add_player(@modric)
+    @croatia.add_player(@vida)
+
+    @world_cup = WorldCup.new(2018, [@france, @croatia])
+  end
+
+  it "exists" do
+    expect(@world_cup).to be_an_instance_of(WorldCup)
+  end
+end
